d922679a8848ec0db1b482b273cc15055b53efaf
= <=>
2023-11-12 15:46:24 +0000

    r

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
-const {createApp} = require("./app")
-const app =  createApp()
+const express = require("express")
 
+const app = express()
+
+
+app.get("/",(req:any,res:any)=>{
+    res.send("hi will")
+})
 app.listen(5000,()=>{
     console.log("ok done")
 })
