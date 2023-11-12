acffbaff4a64162828a8b2d93e8b0eee35de792a
= <=>
2023-11-12 15:52:22 +0000

    r

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
 const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-    res.send("hi will");
-});
+const app = (0, app_1.createApp)();
+// app.get("/",(req:any,res:any)=>{
+//     res.send("hi will")
+// })
 app.listen(5000, () => {
     console.log("ok done");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
+import { createApp } from "./app"
+
 const express = require("express")
 
-const app = express()
-
-
-app.get("/",(req:any,res:any)=>{
-    res.send("hi will")
-})
+const app = createApp();
+// app.get("/",(req:any,res:any)=>{
+//     res.send("hi will")
+// })
 app.listen(5000,()=>{
     console.log("ok done")
 })
