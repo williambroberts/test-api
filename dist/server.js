843691c1518c7692555d452c7b9000e0888c074e
= <=>
2023-11-12 14:54:31 +0000

    ts

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("hi will");
+});
+app.listen(5000, () => {
+    console.log("ok done");
+});
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 
-app.get("/",(req,res)=>{
+app.get("/",(req:any,res:any)=>{
     res.send("hi will")
 })
 app.listen(5000,()=>{
